336b8799237d3614d0f74ef4a85c7a4c70ce6993
Edison0ng <joylantang505@gmail.com>
2026-06-29 19:47:48 +0800

    feat: initial commit - frontend Blade Sistem Libra

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\Route;
-
-// Mengarahkan halaman utama pasca login ke DashboardController
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
